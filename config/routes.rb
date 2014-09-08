83dd5827f179eda768d6d93a94fe679fe307a248
Carl Zulauf <carl@linkleaf.com>
2014-09-08 01:06:00 -0500

    Routes for omniauth

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resource :user, only: [:show, :destroy]
+  match "auth/:provider/callback", to: "users#callback", via: [:get, :post]
+
   root 'welcome#index'
 end
