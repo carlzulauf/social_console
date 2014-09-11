b8f227639f2e6d94451498a68dedeb9c36aba46a
Carl Zulauf <carl@linkleaf.com>
2014-09-11 15:12:30 -0500

    Add ability to delete credentials

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :credentials
+
   resource :user, only: [:show, :destroy]
   match "auth/:provider/callback", to: "users#callback", via: [:get, :post]
 
